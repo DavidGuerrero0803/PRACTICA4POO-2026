896d6eda18868da7dca9219a14fe32d22e20feaf
David <mario.guerrero.marquez@uabc.edu.mx>
2026-04-26 14:14:38 -0700

    Creada la clase Tablero, que contiene principalmente el color (del tablero) y un ArrayList anidado para representar las filas y columnas dentro de una colección.

---
+++ src/main/java/uabc/david/practica4poo2k26/Tablero.java
@@ -0,0 +1,31 @@
+package uabc.david.practica4poo2k26;
+
+import java.util.ArrayList;
+
+public class Tablero {
+    private ArrayList<ArrayList<String>> cuadricula;
+    private String colorTablero;
+
+    public Tablero(String color) {
+        this.colorTablero = color;
+        this.cuadricula = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            ArrayList<String> fila = new ArrayList<>();
+            for (int j = 0; j < 4; j++) {
+                // V = vacío.
+                fila.add("V");
+            }
+            this.cuadricula.add(fila);
+        }
+    }
+
+    public String getPosicion(Posicion posicion) {
+        return cuadricula.get(posicion.getFila()).get(posicion.getColumna());
+    }
+
+    public void setPosicion(Posicion posicion, String pieza) {
+        cuadricula.get(posicion.getFila()).set(posicion.getColumna(), pieza);
+    }
+
+}
