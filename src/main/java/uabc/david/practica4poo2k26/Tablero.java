eb46b41fc1052b861557b8ec2406d6fc743a494a
David <mario.guerrero.marquez@uabc.edu.mx>
2026-04-27 21:20:53 -0700

    Modificaciones en la clase Posicion y Tablero, adición de métodos y de variables estáticas finales.

--- src/main/java/uabc/david/practica4poo2k26/Tablero.java
+++ src/main/java/uabc/david/practica4poo2k26/Tablero.java
@@ -6,14 +6,26 @@ public class Tablero {
     private ArrayList<ArrayList<String>> cuadricula;
     private String colorTablero;
 
+    public static final String VACÍA = "V";
+    public static final String NEGRA = "N";
+    public static final String BLANCA = "B";
+
+    public static final String COLOR_NEGRO = "NEGRO";
+    public static final String COLOR_BLANCO = "BLANCO";
+
+    public static final int TAMAÑO = 4;
+
     public Tablero(String color) {
         this.colorTablero = color;
         this.cuadricula = new ArrayList<>();
+        inicializarCuadricula();
+    }
 
-        for (int i = 0; i < 4; i++) {
+    private void inicializarCuadricula() {
+        for (int i = 0; i < TAMAÑO; i++) {
             ArrayList<String> fila = new ArrayList<>();
-            for (int j = 0; j < 4; j++) {
-                fila.add("V");
+            for (int j = 0; j < TAMAÑO; j++) {
+                fila.add(VACÍA);
             }
             this.cuadricula.add(fila);
         }
@@ -23,12 +35,20 @@ public class Tablero {
         return this.colorTablero;
     }
 
+    public String getColorOpuesto() {
+        return colorTablero.equals(COLOR_BLANCO) ? COLOR_NEGRO : COLOR_BLANCO;
+    }
+
     public String getPosicion(Posicion posicion) {
         return cuadricula.get(posicion.getFila()).get(posicion.getColumna());
     }
 
-    public void setPosicion(Posicion posicion, String pieza) {
-        cuadricula.get(posicion.getFila()).set(posicion.getColumna(), pieza);
+    public void setPosicion(Posicion posicion, String piedra) {
+        cuadricula.get(posicion.getFila()).set(posicion.getColumna(), piedra);
+    }
+
+    public boolean estaVacia(Posicion posicion) {
+        return getPosicion(posicion).equals(VACÍA);
     }
 
 }
